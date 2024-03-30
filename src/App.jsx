9bb99cd0a981eaa8f1df110e8bf5455a2eef6f93
Allen-Josu <allenalcakaparambil@gmail.com>
2024-03-30 23:31:35 +0530

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css'
+import "./App.css";
+import Footer from "./components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-
   return (
     <>
-
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
